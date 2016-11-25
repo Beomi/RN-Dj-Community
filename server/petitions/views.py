2f2085cf41ffd2775dea21e4cfa9ed54a6ef7451
Beomi <latheledusjp@gmail.com>
2016-11-25 23:20:27 +0900

    add check student url

--- server/petitions/views.py
+++ server/petitions/views.py
@@ -1,5 +1,22 @@
 # django
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # core
-from ..core.views import
+from ..core.views import _check_if_student
+
+
+def index(request):
+    pass
+
+def check_student(request):
+    if (request.method == 'POST') and request.is_ajax():
+        return JsonResponse({
+            'result': _check_if_student(
+                user_id=request.POST.get('user_id'),
+                user_pw=request.POST.get('user_pw')
+            )
+        })
+    return JsonResponse({
+        'result': False
+    })
